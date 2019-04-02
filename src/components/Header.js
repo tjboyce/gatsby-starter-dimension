5bc567de39f7b305c48b008f0c74a31b23ae143b
Todd Boyce Jr <tjboyce@me.com>
2019-04-02 11:04:42 -0500

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,8 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
+                <h1>Tj Boyce</h1>
+                <p>Full Stack Software Engineer and lover of people<a href="https://html5up.net">HTML5 UP</a> and released<br />
                 for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
             </div>
         </div>
